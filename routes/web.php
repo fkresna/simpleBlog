0f2e95425d789b6320901915e7aff6b8b40bf956
Freddy <fkresna@gmail.com>
2018-03-16 17:39:16 -0400

    edit routes for post and welcome page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/posts/create','PostController@create');
+
+Route::post('/posts','PostController@store');
+
+Route::get('posts/{post}','PostController@show');
