0a61381b09cfe049a33ad2322dc8c89c6f4a48d0
Freddy Kresna Tjoenedi <fkresna@gmail.com>
2018-03-17 00:17:47 -0400

    Add post features

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,18 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\User;
 
 class PostController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth')->except('index','show');
+    }
+
 	public function index() {
-		$posts = Post::all();
+		$posts = Post::orderBy('created_at','desc')->get();
 
 		return view('welcome',compact('posts'));
 	}
 
-	public function show() {
-		return view('posts.show');
+	public function show($id) {
+
+        $post = Post::find($id);
+		return view('posts.show',compact('post'));
 	}
+
+    public function showUser($user_id) {
+
+        $posts = Post::where('user_id',$user_id)->get();
+        $user = User::find($user_id);
+
+        $data = array(
+            'user' => $user,
+            'posts' => $posts
+        );
+
+        return view('posts.showUser',$data);
+    }
 
     public function create() {
     	return view('posts.create');
@@ -26,11 +46,13 @@ class PostController extends Controller
     	$this->validate(request(), [
     		'title' => 'required',
     		'content' => 'required'
+
     	]);
 
     	$post = new Post;
     	$post->title = request('title');
     	$post->content = request('content');
+        $post->user_id = auth()->id();
     	$post->save();
 
     	return redirect('/');
